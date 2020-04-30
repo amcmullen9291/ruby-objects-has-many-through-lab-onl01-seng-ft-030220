65adc1861556bd5cc369e7c741b323648b229ea0
anthony mcmullen <jondouglasheit@gmail.com>
2020-04-30 09:32:25 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,15 @@
 class Artist
 	attr_accessor :name
-
+@@all = []
 	def initialize(name)
 		@name = name
-		@songs = []
+		@@all << self
 	end
 
+
+def all 
+@@all
+end 
 	def add_song(song)
 		@songs << song
 		song.artist = self
